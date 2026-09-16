b13a9b55b91a6d7d48eb34733a342b3747dd983d
quarnel-dev <quarnel.dev@gmail.com>
2026-09-16 12:56:17 +0300

    feat: add HttpClient types

---
+++ src/types/httpClient.types.ts
@@ -0,0 +1,12 @@
+export type HttpHeaders = Record<string, string> | Headers | [string, string][]
+
+export interface HttpClientOptions {
+  baseURL?: string
+  headers?: HttpHeaders
+}
+
+export interface RequestOptions extends Omit<RequestInit, 'body' | 'method' | 'headers'> {
+  headers?: HttpHeaders
+}
+
+export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
