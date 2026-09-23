6b4379e349b45db26ea4e2c3f7bc2ff6951cf444
quarnel-dev <quarnel.dev@gmail.com>
2026-09-23 14:51:44 +0300

    feat: export public types from index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { HttpClient } from './HttpClient.js'
 export { HttpError } from './errors/HttpError.error.js'
+
+export type { HttpClientOptions, RequestOptions, HttpMethod, HttpHeaders, RequestBody } from './types/httpClient.types.js'
+export type { RetryOptions } from './types/retry.types.js'
